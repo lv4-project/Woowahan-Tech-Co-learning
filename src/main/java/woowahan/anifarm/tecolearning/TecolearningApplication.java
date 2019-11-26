33c206478c7bfa68ada1d35f4349949e65c4ef78
이두호 <ddu0422@naver.com>
2019-11-26 17:42:02 +0900

    feat: 초기 커밋

---
+++ src/main/java/woowahan/anifarm/tecolearning/TecolearningApplication.java
@@ -0,0 +1,13 @@
+package woowahan.anifarm.tecolearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecolearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TecolearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/woowahan/anifarm/tecolearning/TecolearningApplicationTests.java
@@ -0,0 +1,13 @@
+package woowahan.anifarm.tecolearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecolearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
