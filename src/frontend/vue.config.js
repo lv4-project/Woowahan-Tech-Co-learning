ed7757a52fe53e6605644edb8ffc7b7161ddb635
deocksoo <dusol92@snu.ac.kr>
2019-11-29 06:52:09 +0900

    feat: Vue 개발 서버에 프록시 설정 추가

--- src/frontend/vue.config.js
+++ src/frontend/vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
-}
+  ],
+  devServer: {
+    proxy: {
+      '^/api' : {
+        target: 'http://localhost:9000',
+        ws: true,
+        changeOrigin: true
+      },
+    },
+  },
+};
