34033a6f22f04d047bde2528ec8a89c65211ec63
deocksoo <dusol92@snu.ac.kr>
2019-11-29 11:47:12 +0900

    feat: App.vue 기본 템플릿 작성

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,10 +1,46 @@
 <template>
   <v-app>
-    <StudyRecruitment/>
-    <OngoingStudy/>
-    <FinishedStudy/>
-    <MyPage/>
-    <ParticipatingStudy/>
+    <v-row>
+      <v-toolbar dense>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+        <v-toolbar-title>Title</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>mdi-dots-vertical</v-icon>
+        </v-btn>
+      </v-toolbar>
+    </v-row>
+    <v-row>
+      <component :is="view"></component>
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-btn @click="view = 'StudyRecruitment'">모집</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn @click="view = 'OngoingStudy'">진행중</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn @click="view = 'FinishedStudy'">완료</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn @click="view = 'MyPage'">MyPage</v-btn>
+      </v-col>
+      <v-col>
+        <v-btn @click="view = 'ParticipatingStudy'">MyStudy</v-btn>
+      </v-col>
+    </v-row>
   </v-app>
 </template>
 
@@ -25,7 +61,7 @@
       ParticipatingStudy,
     },
     data: () => ({
-      //
+      view: 'StudyRecruitment'
     }),
   };
 </script>
